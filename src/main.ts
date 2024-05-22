698437c3eaa58b1828b20f096ec30b6ce9e0c533
muller-g <gabriel_muller98@hotmail.com>
2024-05-22 13:42:53 -0300

    Refactor .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import app from '../src/infra/server';
 import LoginController from './infra/http/api/LoginController';
 import UserController from './infra/http/api/UserController';
+import logger from './service/WinstonLogger';
 
 new UserController();
 new LoginController();
 
 app.listen(3001, () => {
-    return console.log(`Express is listening at http://localhost:3001`);
+    logger.info(`Express is listening at http://localhost:3001`);
 });
